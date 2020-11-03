46c6800c304981c1537e926a8ac168625bcacc8b
Rakshitha86 <73781046+Rakshitha86@users.noreply.github.com>
2020-11-03 15:37:59 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,14 @@
+class Even{
+public static void main(String args[]){
+int d[]={1,2,3,4,5,6,7,8,9,10,11,12};
+int i;
+for(i=0;i<12;i++){
+if(d[i]%2==0){
+System.out.println(d[i]+ "=even");
+}
+else{
+System.out.println(d[i]+ "=odd");
+}
+}
+}
+}
